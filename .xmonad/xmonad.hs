268363ce60cbea35f3c9960182ea299e0dcc499b
Lucas Bergman <lucas@bergmans.us>
2014-03-06 14:37:20 -0600

    xmonad: Switch to GNOME

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,10 @@
 module Main (main) where
 
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.SetWMName
-import XMonad.Layout.PerWorkspace
+import XMonad.Config.Desktop (desktopLayoutModifiers)
+import XMonad.Config.Gnome (gnomeConfig, gnomeRun)
+import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Util.EZConfig (additionalKeysP, removeKeysP)
 
 myLayout = onWorkspace "1" (Mirror unevenTiledLayout) defaultLayouts
   where
@@ -14,9 +15,19 @@ myLayout = onWorkspace "1" (Mirror unevenTiledLayout) defaultLayouts
     nmaster = 1
     delta = 3/100
 
-main = xmonad =<< xmobar defaultConfig
+-- If we're using the mod4 as a modifier key, then move the binding for the
+-- GNOME run-program dialog from mod-p to mod-shift-p. GNOME steals mod4-p
+-- unconditionally and unconfigurably to reconfigure your displays, based on a
+-- crazy recommendation from Microsoft:
+-- https://bugs.launchpad.net/ubuntu/+source/gnome-settings-daemon/+bug/694910
+fixGnomeRunKey :: XConfig l -> XConfig l
+fixGnomeRunKey conf@XConfig { modMask = mod4Mask } =
+    conf `removeKeysP` ["M-p"] `additionalKeysP` [("M-S-p", gnomeRun)]
+fixGnomeRunKey conf = conf
+
+main = xmonad $ fixGnomeRunKey gnomeConfig
   { modMask = mod4Mask
-  , layoutHook = myLayout
   , startupHook = setWMName "LG3D"
   , terminal = "exec gnome-terminal"
+  , layoutHook = desktopLayoutModifiers myLayout
   }
