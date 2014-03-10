fffbbee1a70d5dd6a91c66decfe9bedc0b39d7d9
Lucas Bergman <lucas@bergmans.us>
2014-03-10 10:07:14 -0500

    emacs: Change Haskell indent style to 4 spaces

--- elisp/slb-pkg-haskell-mode.el
+++ elisp/slb-pkg-haskell-mode.el
@@ -1,2 +1,5 @@
 (slb-hack-mode haskell-mode-hook nil
+  (setq haskell-indentation-layout-offset 4
+        haskell-indentation-left-offset 4
+        haskell-indentation-ifte-offset 4)
   (turn-on-haskell-indentation))
