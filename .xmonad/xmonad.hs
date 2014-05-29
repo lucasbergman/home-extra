f3a324f8a5f0586302dedf7e4ad6e6d27ce4ceb0
Lucas Bergman <lucas@bergmans.us>
2014-05-29 14:19:00 -0500

    xmonad: Update to use xmobar and not gnome-panel

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,9 +2,10 @@ module Main (main) where
 
 import XMonad
 import XMonad.Config.Desktop (desktopLayoutModifiers)
-import XMonad.Config.Gnome (gnomeConfig, gnomeRun)
+import XMonad.Config.Gnome (gnomeConfig)
+import XMonad.Hooks.DynamicLog (xmobar)
 import XMonad.Layout.PerWorkspace (onWorkspace)
-import XMonad.Util.EZConfig (additionalKeysP, removeKeysP)
+import XMonad.Util.EZConfig (additionalKeysP)
 
 myLayouts = onWorkspace "1" (primary ||| defaultLayouts) defaultLayouts
   where
@@ -13,19 +14,16 @@ myLayouts = onWorkspace "1" (primary ||| defaultLayouts) defaultLayouts
     tiledLayout = Tall 1 delta 0.5
     delta = 0.03
 
--- If we're using the mod4 as a modifier key, then move the binding for the
--- GNOME run-program dialog from mod-p to mod-shift-p. GNOME steals mod4-p
--- unconditionally and unconfigurably to reconfigure your displays, based on a
--- crazy recommendation from Microsoft:
--- https://bugs.launchpad.net/ubuntu/+source/gnome-settings-daemon/+bug/694910
-fixGnomeRunKey :: XConfig l -> XConfig l
-fixGnomeRunKey conf@XConfig { modMask = mod4Mask } =
-    conf `removeKeysP` ["M-p"] `additionalKeysP` [("M-S-p", gnomeRun)]
-fixGnomeRunKey conf = conf
-
-main = xmonad $ fixGnomeRunKey gnomeConfig
-           { modMask = mod4Mask
-           , borderWidth = 3
-           , layoutHook = desktopLayoutModifiers myLayouts
-           , terminal = "exec urxvt"
-           }
+main = xmonad =<< xmobar config
+  where
+    config = myGnomeConfig `additionalKeysP` myKeys
+    myGnomeConfig = gnomeConfig
+        { modMask = mod4Mask
+        , borderWidth = 3
+        , layoutHook = desktopLayoutModifiers myLayouts
+        , terminal = "exec gnome-terminal"
+        }
+    myKeys =
+        [ ("C-M1-l", spawn "gnome-screensaver-command --lock")
+        , ("M-S-q", spawn "gnome-session-quit")
+        ]
