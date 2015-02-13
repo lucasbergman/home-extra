4b23b0c798b97d1467dbc5e6209492a8d384285e
Lucas Bergman <lucas@bergmans.us>
2015-02-13 12:26:01 -0600

    emacs: Remove remote-host notify from ERC
    This hack was in place to use ssh to run notify-send on a remote host in
    case Emacs/ERC was running somewhere other than the client (so that
    running notify-send on the Emacs host was useless). This was never very
    robust and basically never worked for work where I need it most, because
    security. I'll have to figure something else out.

--- elisp/slb-erc-utils.el
+++ elisp/slb-erc-utils.el
@@ -1,8 +1,5 @@
 (require 'erc)
 
-(defvar *slb-display-host* nil
-  "Host name to send display notifications, or nil for local host")
-
 (defun slb-erc-global-notify (matched-type nick msg)
   (when (and (or (eq matched-type 'current-nick)
                  (eq matched-type 'keyword))
@@ -15,14 +12,6 @@
                         (format "%s mentioned you in IRC %s"
                                 (car (split-string nick "!"))
                                 (or (erc-default-target) "#unknown"))))))
-
-      ;; If we send notifications to another host, we have to do the SSH thing
-      ;; and double-quote the command, because fuck you that's why.
-      (unless (null *slb-display-host*)
-        (setq cmd (format "ssh -T %s env DISPLAY=:0 %s"
-                          *slb-display-host*
-                          (shell-quote-argument cmd))))
-
       (shell-command cmd))))
 
 (provide 'slb-erc-utils)
