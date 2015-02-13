46998c402b99bac29fc617eb6a6c3da0af1528cc
Lucas Bergman <lucas@bergmans.us>
2015-02-13 12:25:34 -0600

    emacs: Diminish a few things from the mode line

--- elisp/99-pkg-magit.el
+++ elisp/99-pkg-magit.el
@@ -1,3 +1,4 @@
 (use-package magit
   :ensure t
+  :diminish magit-auto-revert-mode
   :commands magit-status)
--- elisp/99-pkg-whitespace.el
+++ elisp/99-pkg-whitespace.el
@@ -1,5 +1,6 @@
 (use-package whitespace
   :demand t
+  :diminish whitespace-mode
   :config (setq whitespace-style '(face trailing space-after-tab
                                    space-before-tab lines-tail)
                 whitespace-line-column 78))
