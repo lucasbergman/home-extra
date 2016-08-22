2855ab4ec5a0c08c8d32eda24718bb8a85abf227
Lucas Bergman <lucas@bergmans.us>
2016-08-22 07:35:51 -0500

    emacs: Delay loading of utilities for JS2-MODE

--- elisp/99-pkg-js2-mode.el
+++ elisp/99-pkg-js2-mode.el
@@ -1,11 +1,10 @@
-(require 'slb-js-utils)
-(require 'slb-util)
-
 (use-package js2-mode
   :ensure t
   :commands js2-mode
   :mode (("\\.js\\'" . js2-mode)
          ("\\.jsx\\'" . js2-jsx-mode))
   :config (progn
+	    (require 'slb-js-utils)
+	    (require 'slb-util)
 	    (slb-hack-mode js2-mode-hook)
 	    (add-hook 'js2-mode-hook #'slb-js-hack)))
