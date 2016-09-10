6ba0d7a32f2ce8e9c09d2074450ddaf9908282e2
Lucas Bergman <lucas@bergmans.us>
2016-09-10 20:21:21 +0000

    emacs: Fix EDITORCONFIG-MODE for Emacs Lisp
    The Emacs plugin for editorconfig gets the semantics of Lisp indentation
    wrong, so this backs out that bogus setting.

--- elisp/99-pkg-editorconfig.el
+++ elisp/99-pkg-editorconfig.el
@@ -1,3 +1,14 @@
 (use-package editorconfig
   :ensure t
-  :config (editorconfig-mode 1))
+  :config
+  (progn
+    ;; The Emacs package for editorconfig sets LISP-INDENT-OFFSET to the value
+    ;; of indent_size in editorconfig settings; that gives bogus indentation
+    ;; when (e.g.) Lisp args need to be aligned with each other. I think the
+    ;; lossage is unique to the Emacs package, so I fix it here instead of
+    ;; (say) setting indent_size to nil in editorconfig settings.
+    (add-hook 'editorconfig-custom-hooks
+      #'(lambda (unused-props)
+          (when (eq major-mode 'emacs-lisp-mode)
+            (setq-local lisp-indent-offset nil))))
+    (editorconfig-mode 1)))
